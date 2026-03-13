ec46b64d90bd7f6e63cefd94f0a72fc11852b849
Vladimir <v.a.lesnykh@gmail.com>
2026-03-13 12:51:20 +0300

    fix: revert click area to track only, text label is not interactive

--- source/ToggleSwitch.pas
+++ source/ToggleSwitch.pas
@@ -453,7 +453,7 @@ end;
 procedure TFluentToggleSwitch.MouseDown(Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
 begin
   inherited;
-  if Button = mbLeft then
+  if (Button = mbLeft) and PtInRect(GetTrackRect, Point(X, Y)) then
   begin
     FPressed := True;
     Invalidate;
@@ -465,7 +465,7 @@ begin
   if (Button = mbLeft) and FPressed then
   begin
     FPressed := False;
-    if PtInRect(ClientRect, Point(X, Y)) then
+    if PtInRect(GetTrackRect, Point(X, Y)) then
       Toggle;
     Invalidate;
   end;
